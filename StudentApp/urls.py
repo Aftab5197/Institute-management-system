59813c17e3b8ebf146c4e4f24d2c7e1442d63c89
Aftab5197 <138246963+Aftab5197@users.noreply.github.com>
2023-11-29 17:04:56 +0530

    Add files via upload

---
+++ StudentApp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from StudentApp import views
+
+urlpatterns= [
+    path('',views.loginfun,name='login'),
+    path('logout',views.logoutfun,name='logout'),
+    path('register',views.registerfun,name='register'),
+    path('home',views.home,name='home'),
+    path('add',views.add,name='add'),
+    path('edit/<int:id>',views.edit,name='edit'),
+    path('delete/<int:id>',views.deletefun,name='delete'),
+    path('dummy',views.dummy,name='dummy')
+]
